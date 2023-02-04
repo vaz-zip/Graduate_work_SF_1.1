52f9d2a5a7e006e6639fe0a622b0e82ebc2eb657
KONSNANTIN SHIRYAEV <97960873+vaz-zip@users.noreply.github.com>
2023-02-04 18:12:20 +0400

    Update settings.py

--- My_Silant/My_Silant/settings.py
+++ My_Silant/My_Silant/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=k4om=6$8tbnfq9jtj47tpj-1#7lhs2$liz0ssz5h16_huj8g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
